e54dde5189c1d663055ff89a6b871c31f3c03fbb
Manzanit0 <unico.javier@gmail.com>
2022-12-23 16:11:24 +0100

    Bump down due to tooling

--- nous/mix.exs
+++ nous/mix.exs
@@ -5,7 +5,7 @@ defmodule Nous.MixProject do
     [
       app: :nous,
       version: "0.1.0",
-      elixir: "~> 1.14.2",
+      elixir: "~> 1.13.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
