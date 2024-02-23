0aaba1c34490caf3631ea3cef86115f1a9c109da
Javier García <unico.javier@gmail.com>
2024-02-23 14:45:37 +0100

    Update deployment tooling

--- nous/mix.exs
+++ nous/mix.exs
@@ -17,7 +17,7 @@ defmodule Nous.MixProject do
   def application do
     [
       mod: {Nous.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
@@ -28,19 +28,19 @@ defmodule Nous.MixProject do
     [
       {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.11"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
+      {:floki, ">= 0.35.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:aws, "~> 0.13.0"},
-      {:hackney, "~> 1.17"}
+      {:aws, "~> 0.14.1"},
+      {:hackney, "~> 1.17"},
     ]
   end
 
@@ -48,8 +48,9 @@ defmodule Nous.MixProject do
     [
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+      # TODO: disable for now; we don't have tests hitting the DB anyways.
+      # test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
